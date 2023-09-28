76b730f27b1046febec0694494b89021780c4a17
Tony Tang <tangchang21@gmail.com>
2023-09-28 10:47:29 +0800

    quic echo server & client

--- quic/echo_client/main.go
+++ quic/echo_client/main.go
@@ -1,6 +1,44 @@
 package main
 
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"io"
+
+	"github.com/quic-go/quic-go"
+)
+
+// https://github.com/quic-go/quic-go/blob/master/example/echo/echo.go
+
 func main() {
-	// WIP
-	print("hello")
+	tlsConf := &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"quic-echo-example"},
+	}
+	conn, err := quic.DialAddr(context.Background(), "localhost:4433", tlsConf, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	stream, err := conn.OpenStreamSync(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	message := "hello"
+	fmt.Printf("Client: Sending '%s'\n", message)
+	_, err = stream.Write([]byte(message))
+	if err != nil {
+		panic(err)
+	}
+
+	buf := make([]byte, len(message))
+	_, err = io.ReadFull(stream, buf)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Client: Got '%s'\n", buf)
+
+	conn.CloseWithError(0, "")
 }
--- quic/echo_server/main.go
+++ quic/echo_server/main.go
@@ -2,33 +2,76 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
+	"crypto/rsa"
 	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
-	"net"
+	"io"
+	"math/big"
 
 	"github.com/quic-go/quic-go"
 )
 
+// https://github.com/quic-go/quic-go/blob/master/example/echo/echo.go
+
 func main() {
-	udpConn, err := net.ListenUDP("udp", &net.UDPAddr{Port: 4433})
-	if err != nil {
-		panic(fmt.Sprintf("create udp listener failed: %s", err))
-	}
+	echoServer()
+}
 
-	// WIP config tls certs
-	ln, err := quic.Listen(udpConn, &tls.Config{}, nil)
+// Start a server that echos all data on the first stream opened by the client
+func echoServer() error {
+	listener, err := quic.ListenAddr("localhost:4433", generateTLSConfig(), nil)
 	if err != nil {
-		panic(fmt.Sprintf("create quic listener failed: %s", err))
+		return err
 	}
-
 	for {
-		conn, err := ln.Accept(context.Background())
+		println("start to accept connection")
+		conn, err := listener.Accept(context.Background())
 		if err != nil {
-			panic(fmt.Sprintf("accept quic connection failed: %s", err))
+			return err
 		}
+		println("did accept one connection")
+		stream, err := conn.AcceptStream(context.Background())
+		if err != nil {
+			panic(err)
+		}
+		// Echo through the loggingWriter
+		io.Copy(Logger{stream}, stream)
+	}
+}
 
-		// WIP impl echo
+type Logger struct {
+	io.Writer
+}
 
-		conn.CloseWithError(1, "WIP")
+func (w Logger) Write(b []byte) (int, error) {
+	fmt.Printf("Server got: '%s'\n", string(b))
+	return w.Writer.Write(b)
+}
+
+func generateTLSConfig() *tls.Config {
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		panic(err)
+	}
+
+	template := x509.Certificate{SerialNumber: big.NewInt(1)}
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	if err != nil {
+		panic(err)
+	}
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+
+	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		panic(err)
+	}
+
+	return &tls.Config{
+		Certificates: []tls.Certificate{tlsCert},
+		NextProtos:   []string{"quic-echo-example"},
 	}
 }
